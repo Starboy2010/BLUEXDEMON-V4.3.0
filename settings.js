3b36a7778fc60f276f21aec193a0af50dde3467c
Starboy2010 <166742048+Starboy2010@users.noreply.github.com>
2025-01-17 20:45:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const version = require("@whiskeysockets/baileys/package.json").version
 
 
 //Input number for pair code
-global.pairingNumber = "2348033956837"
+global.pairingNumber = "234706492081"
 
 
 
@@ -36,7 +36,7 @@ global.pairingCode = true
 global.runWith = "𝙽𝙾𝙳𝙴.𝙹𝚂"
 global.newsletterJid = "120363304326105871@newsletter"
 global.newsletterName = "𝐃𝐄𝐌𝐎𝐍 𝐗 𝐙𝐎𝐑𝐎 𝐕𝟒"
-global.caption = "𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝙱𝙻𝚄𝙴𝙳𝙴𝙼𝙾𝙽"
+global.caption = "𝙿𝚘𝚠𝚎𝚛𝚎𝚍 𝚋𝚢 𝐒𝐓𝐀𝐑 𝐓𝐄𝐂𝐇™᭄ㅤ ㅤ ㅤ ㅤ  ㅤ"
 global.ownerName = "𝐁𝐋𝐔𝐄 𝐃𝐄𝐌𝐎𝐍"
 global.syt = 'https://whatsapp.com/channel/0029VaozjHRLI8YOzV5j5y2m'
 global.sgc = 'https://whatsapp.com/channel/0029VaozjHRLI8YOzV5j5y2m'
